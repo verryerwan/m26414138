d5295933d463ac7ff4f8e74d2c8f1cbd3d36c4a9
verryerwan <m26414138@john.petra.ac.id>
2016-09-11 16:01:58 +0700

    segitiga kebalik

---
+++ code4.sh
@@ -0,0 +1,10 @@
+read a;
+         for ((i=a;i>0;i--))
+        {
+                for ((j=0;j<i;j++))
+                {
+                        echo -n "*";
+                }
+                echo "";
+        }
+
