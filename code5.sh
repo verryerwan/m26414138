37aa0f4a2ca18ae72c12dccc82747368391e7803
verryerwan <m26414138@john.petra.ac.id>
2016-09-11 16:05:17 +0700

    try code

---
+++ code5.sh
@@ -0,0 +1,13 @@
+read a;
+for ((i=1;i<=a;i++))
+        {
+                for ((j=1;j<=i;j++))
+                {
+                        echo -n "*";
+                }
+                echo "";
+        }
+
+
+
+
