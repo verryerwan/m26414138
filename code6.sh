8f1dfb88952daa41f5d181b831df63c6909e523c
verryerwan <m26414138@john.petra.ac.id>
2016-09-14 23:32:20 +0700

    still error

--- code6.sh
+++ code6.sh
@@ -2,3 +2,25 @@ let i;
 let n=0;
 let a;
 read i;
+until ((n >= i * 2 + 1))
+do
+        for ((a=0;a<=i * 2 + 2;a++))
+                {
+                        if (($n % 2 == 0))
+                        then
+                                if (($a % 2 == 0))
+                                then
+                                        echo -n "+";
+                                else
+                                        echo -n "-";
+                                fi
+                        else
+                                if (($a % 2 == 0))
+                                then
+                                        echo  -n "|";
+                                else
+                                        echo  -n $i;
+                                fi
+                        fi
+                }
+
